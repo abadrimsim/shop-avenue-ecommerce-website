a2a83bb94d61ec5d7ca7e040ee3af8823e13a739
Angel <abadrimsim@gmail.com>
2022-02-07 23:02:25 +0800

    Update tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,6 @@
 module.exports = {
 	mode: 'jit',
-	content: [
-		'./src/pages/*.{js,ts,jsx,tsx}',
-		'./src/pages/**/*.{js,ts,jsx,tsx}',
-		'./src/components/*.{js,ts,jsx,tsx}',
-	],
+	content: ['./src/pages/*.js', './src/pages/**/*.js', './src/components/*.js'],
 	darkMode: true,
 	theme: {
 		extend: {
